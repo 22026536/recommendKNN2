eaebd93c43796db0c7d301f4b5b73a5e53fa0aa2
22026536 <22026536>
2024-12-10 20:27:32 +0700

    quang

--- knn2.py
+++ knn2.py
@@ -13,7 +13,7 @@ from fastapi.middleware.cors import CORSMiddleware
 # Thêm middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Cho phép tất cả origin
+    allow_origins=["https://animetangobackend.onrender.com"],  # Cho phép tất cả origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
